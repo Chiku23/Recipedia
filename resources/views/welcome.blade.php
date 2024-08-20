ec8a6df6853d5ab08a67351155fc5cf242a056fe
Chiku <subhakantacs2000@gmail.com>
2024-08-20 21:36:49 +0530

    Initial Setup.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Recipedia</title>
+        <link rel="stylesheet" href="{{asset('css/tailwind.css')}}">
+        <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+    </head>
+    <body class="font-mono">
+        Homepage
+    </body>
+</html>
